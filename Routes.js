8f456fed1db934b068130c58407f5c708287fbad
sourabhgera8 <sourabhgera8@gmail.com>
2021-10-18 21:07:27 +0530

    init eazy erp project

---
+++ Routes.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './apps/screens/SplashScreen';
+
+const Stack = createNativeStackNavigator();
+  
+  function Routes() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SplashScreen">
+
+          <Stack.Screen  options={{headerShown: false}}  name="SplashScreen" component={SplashScreen} />
+      
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
+  export default Routes;
---
+++ apps/screens/SplashScreen.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default function SplashScreen({ route, navigation }) {
+
+    useEffect(() => {
+        setTimeout(() => {
+            // navigation.navigate('Loginscreen');
+        }, 1000);
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textStyle}>
+                {"Eazy ERP"}
+            </Text>
+
+            <View style={styles.imagViewStyle}  >
+                <Image
+                    style={styles.imageStyle}
+                    resizeMode="contain"
+                    source={require('../assets/eazyfooterlogo.png')}
+                />
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000000",
+    },
+    textStyle: {
+        marginTop: 20,
+        color: "white",
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 40
+    },
+    imagViewStyle: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageStyle: { 
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
